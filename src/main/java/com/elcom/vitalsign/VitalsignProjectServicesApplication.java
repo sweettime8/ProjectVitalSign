f33d8dc641db10e2861a3df8a56208125d550efe
unknown <nghduc91@gmail.com>
2020-11-04 16:57:16 +0700

    first commit

---
+++ src/main/java/com/elcom/vitalsign/VitalsignProjectServicesApplication.java
@@ -0,0 +1,13 @@
+package com.elcom.vitalsign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitalsignProjectServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VitalsignProjectServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elcom/vitalsign/VitalsignProjectServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elcom.vitalsign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VitalsignProjectServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
